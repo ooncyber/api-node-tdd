ca2515ef9ce203f61ee14e2783c6e63266036c2b
Gabriel Rodrigues <gabriel.brutu@gmail.com>
2020-12-29 11:23:15 -0300

    Adicionado supertest para testar aplicacao online e novos testes

--- test/jest.test.js
+++ test/jest.test.js
@@ -6,5 +6,16 @@ test('Devo conhecer as principais assertivas do jest', () => {
   expect(number).toBe(10)
   expect(number).toEqual(10)
   expect(number).toBeGreaterThan(9)
-  expect(number).toBeLessThan(9)
+  expect(number).toBeLessThan(11)
 });
+
+test("Devo saber trabalhar com objetos", () => {
+  const obj = { name: "John", mail: "john@gmail.com" }
+  expect(obj).toHaveProperty('name');
+  expect(obj).toHaveProperty('name', 'John');
+  expect(obj.name).toBe('John');
+
+  const obj2 = { name: "John", mail: "john@gmail.com" }
+
+  expect(obj).toEqual(obj2);
+})
